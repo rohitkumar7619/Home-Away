7ec16b405213455d60426ad4b955d12035cdfcce
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-04 09:11:05 +0530

    listing complet

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const { render } = require("ejs");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/homeaway";
 
@@ -17,22 +19,31 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("hi,i ma root");
 });
 
-app.get("/testListing", async (req, res) => {
-  const sampleListing = new Listing({
-    title: "my Home",
-    description: "ji mera ghar hai",
-    Price: 5000,
-    Location: "Kunraghat",
-    country: "India",
-  });
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("sucessful testing");
+//index Rought
+app.get("/listing", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
 });
+
+// app.get("/testListing", async (req, res) => {
+//   const sampleListing = new Listing({
+//     title: "my Home",
+//     description: "ji mera ghar hai",
+//     Price: 5000,
+//     Location: "Kunraghat",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("sucessful testing");
+// });
 
 app.listen(8080, () => {
   console.log("server is listing to port 8080");
---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/homeaway";
+
+main()
+  .then(() => {
+    console.log("Connected with DB");
+    initDB(); // Call initDB here
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  try {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+  } catch (err) {
+    console.error("Error initializing data:", err);
+  }
+};
+
+initDB;
