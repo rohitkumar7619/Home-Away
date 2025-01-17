bd8f63267f2f179fc373f0534d6f16d5f372f1e0
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-17 09:23:51 +0530

    logout route is created and working properly

--- routes/user.js
+++ routes/user.js
@@ -55,4 +55,14 @@ router.post(
   }
 );
 
+router.get("/logout", (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "logged you out!");
+    res.redirect("/listings");
+  });
+});
+
 module.exports = router;
