418eb4432ae7068b5943715150817b80c799b980
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-18 15:26:33 +0530

    we have connected cloudinary and start uploading files relatd to listing or images

--- routes/listing.js
+++ routes/listing.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const wrapAysnc = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudconfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAysnc(listingController.index)) //index Rought
-  .post(isLoggedIn, wrapAysnc(listingController.createListing)); //Create Route
+  // .post(isLoggedIn, wrapAysnc(listingController.createListing)); //Create Route
+  .post(upload.single("listing[image]"), (req, res) => {
+    res.send(req.file);
+  });
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
