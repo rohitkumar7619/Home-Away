54ec9133f332bc5c9c7e3d44ee77ba1c5618c9ab
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-18 11:24:57 +0530

    implementing router.route for uses  which will help in reduce the code and make more readable

--- routes/user.js
+++ routes/user.js
@@ -5,29 +5,26 @@ const wrapAsync = require("../utils/wrapAsync");
 const usersControllers = require("../controllers/users.js");
 const { saveRedirectUrl } = require("../middleware");
 
-// Render Signup Form
-router.get("/signup", usersControllers.signupForm);
+router
+  .route("/signup")
+  .get(usersControllers.signupForm) // Render Signup Form
+  .post(wrapAsync(usersControllers.signup)); // Handle Signup Logic
 
-// Handle Signup Logic
-router.post("/signup", wrapAsync(usersControllers.signup));
-
-// Render Login Form
-router.get("/login", usersControllers.loginForm);
-
-// Handle Login Logic
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  (req, res) => {
-    req.flash("success", "Welcome back!");
-    const redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  }
-);
+router
+  .route("/login")
+  .get(usersControllers.loginForm) // Render Login Form
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    (req, res) => {
+      req.flash("success", "Welcome back!");
+      const redirectUrl = res.locals.redirectUrl || "/listings";
+      res.redirect(redirectUrl);
+    }
+  ); // Handle Login Logic
 
 // Logout Route
 router.get("/logout", usersControllers.logout);
