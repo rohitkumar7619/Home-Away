08c0fe21fc30a23521ebd58dae10e1a49eee738c
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-19 11:15:23 +0530

    on show.ejs map is showing and we added a marker in the map which show the locatin of listing

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,11 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: [83.3732, 26.7606], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9, // starting zoom
 });
+
+console.log(coordinates);
+const marker1 = new mapboxgl.Marker({ color: "black" })
+  .setLngLat(coordinates)
+  .addTo(map);
