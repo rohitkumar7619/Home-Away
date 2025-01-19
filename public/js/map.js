7a1564bf130e06f0d6ffd4c38f4240c5c8d17cd6
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-01-19 11:41:23 +0530

    on map popup we have added when someone click in map icon they get a pop with title and location

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,17 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  zoom: 9, // starting zoom
+  center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+  zoom: 7, // starting zoom
 });
 
-console.log(coordinates);
 const marker1 = new mapboxgl.Marker({ color: "black" })
-  .setLngLat(coordinates)
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 })
+      .setHTML(
+        `<h4>${listing.title}</h4><p>Exact Location provided after Booking</p>`
+      )
+      .setMaxWidth("300px")
+  )
   .addTo(map);
