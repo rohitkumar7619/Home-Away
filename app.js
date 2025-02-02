b737a1a2007892644f3acc1810f1f759ab6d02a3
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-02-02 12:54:02 +0530

    some code is deleted from css and other files are also modefied

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static("public"));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
