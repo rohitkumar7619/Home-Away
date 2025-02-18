df959b7bca75912f92db937edcf0882ca12f853d
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-02-18 18:59:02 +0530

    booking model is  created

---
+++ models/booking.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  listing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Listing",
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  guests: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
