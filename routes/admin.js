c0ffddd18963a081811d98e4ae80de6ee7b42fe4
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-04-06 10:51:12 +0530

    In routes we are connecting admin DashBoard Page

--- routes/admin.js
+++ routes/admin.js
@@ -5,3 +5,14 @@ const { isLoggedIn, isAdmin } = require("../middleware");
 const User = require("../models/user");
 const Listing = require("../models/listing");
 const Booking = require("../models/booking");
+
+// Admin Dashboard
+router.get(
+    "/admin/dashboard",
+    isLoggedIn,
+    isAdmin,
+    wrapAsync(async (req, res) => {
+      res.render("admin/dashboard", {
+      });
+    })
+  );
