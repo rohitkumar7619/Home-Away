db079a4cdcaf1ab239dc8a52fdae4da99810d8ab
Rohit Kumar Gupta <rohit76kumargupta@gmail.com>
2025-04-06 10:34:46 +0530

    creating route for admin page

---
+++ routes/admin.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const { isLoggedIn, isAdmin } = require("../middleware");
+const User = require("../models/user");
+const Listing = require("../models/listing");
+const Booking = require("../models/booking");
